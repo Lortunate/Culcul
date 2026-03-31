2da18e19c5414d1dcfa5da33aec3dfc2fb32f404
Lortunate <houvven@gmail.com>
2026-04-01 02:53:04 +0800

    tmp

---
+++ lib/features/live/data/dtos/live_danmu_info_model.dart
@@ -0,0 +1,27 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'live_danmu_info_model.freezed.dart';
+part 'live_danmu_info_model.g.dart';
+
+@freezed
+sealed class LiveDanmuInfoModel with _$LiveDanmuInfoModel {
+  const factory LiveDanmuInfoModel({
+    required String token,
+    @JsonKey(name: 'host_list') required List<LiveDanmuHost> hostList,
+  }) = _LiveDanmuInfoModel;
+
+  factory LiveDanmuInfoModel.fromJson(Map<String, dynamic> json) =>
+      _$LiveDanmuInfoModelFromJson(json);
+}
+
+@freezed
+sealed class LiveDanmuHost with _$LiveDanmuHost {
+  const factory LiveDanmuHost({
+    required String host,
+    @JsonKey(name: 'wss_port') required int wssPort,
+    @JsonKey(name: 'ws_port') required int wsPort,
+  }) = _LiveDanmuHost;
+
+  factory LiveDanmuHost.fromJson(Map<String, dynamic> json) =>
+      _$LiveDanmuHostFromJson(json);
+}
---
+++ lib/features/live/data/dtos/live_room_model.dart
@@ -0,0 +1,40 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'live_room_model.freezed.dart';
+part 'live_room_model.g.dart';
+
+@freezed
+sealed class LiveRoomModel with _$LiveRoomModel {
+  const factory LiveRoomModel({
+    @JsonKey(name: 'roomid') required int roomId,
+    @JsonKey(name: 'uid') required int uid,
+    required String title,
+    required String uname,
+    required String cover,
+    required String face,
+    @JsonKey(name: 'online') required int online,
+    @JsonKey(name: 'area_v2_name') required String areaName,
+    @JsonKey(name: 'area_v2_parent_name') required String parentAreaName,
+    @JsonKey(name: 'link') required String link,
+    @JsonKey(name: 'keyframe') String? keyframe,
+    @JsonKey(name: 'watched_show') WatchedShow? watchedShow,
+    @JsonKey(name: 'is_auto_play') int? isAutoPlay,
+  }) = _LiveRoomModel;
+
+  factory LiveRoomModel.fromJson(Map<String, dynamic> json) =>
+      _$LiveRoomModelFromJson(json);
+}
+
+@freezed
+sealed class WatchedShow with _$WatchedShow {
+  const factory WatchedShow({
+    @JsonKey(name: 'switch') required bool switchStatus,
+    required int num,
+    @JsonKey(name: 'text_small') required String textSmall,
+    @JsonKey(name: 'text_large') required String textLarge,
+    required String icon,
+    @JsonKey(name: 'icon_web') required String iconWeb,
+  }) = _WatchedShow;
+
+  factory WatchedShow.fromJson(Map<String, dynamic> json) => _$WatchedShowFromJson(json);
+}
