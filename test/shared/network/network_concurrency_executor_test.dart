f8a3ef4e18cf1c17bed173ae6ea252eb8b201930
lortunate <houvven@gmail.com>
2026-04-24 01:55:26 +0800

    test: align shared network imports with core paths

--- test/shared/network/network_concurrency_executor_test.dart
+++ test/shared/network/network_concurrency_executor_test.dart
@@ -1,4 +1,4 @@
-import 'package:culcul/shared/errors/app_error.dart';
+import 'package:culcul/core/errors/app_error.dart';
 import 'package:culcul/shared/network/network_concurrency_executor.dart';
 import 'package:culcul/shared/network/network_concurrency_profiles.dart';
 import 'package:flutter_test/flutter_test.dart';
