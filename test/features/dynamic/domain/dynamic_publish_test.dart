1fc2bb04eae89e88f8d84ac4c5e68960dacbf520
lortunate <houvven@gmail.com>
2026-05-13 05:17:37 +0800

    tmp

--- test/features/dynamic/domain/dynamic_publish_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:culcul/features/dynamic/domain/entities/dynamic_publish_command.dart';
-
-void main() {
-  test('dynamic publish contract accepts a feature command object, not File', () {
-    final command = PublishDynamicCommand(text: 'hello', media: []);
-    expect(command.media, isEmpty);
-  });
-}
